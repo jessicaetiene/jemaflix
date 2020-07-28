6a35392b3e43ccdbb3ca7b0f5af28cc7bd923996
Jessica  Almeida <jessica.almeida@bravi.com.br>
2020-07-28 19:21:20 -0300

    Criação de componentes de cadastro

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css'
-import ButtonLink from './components/ButtonLink'
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="JemaFlix logo"/>
-            </a>
+            </Link>
 
-            {/* <ButtonLink className="ButtonLink" href="/">
-                Novo vídeo
-            </ButtonLink> */}
-
-
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/Video">
                 Novo vídeo
             </Button>
         </nav>
